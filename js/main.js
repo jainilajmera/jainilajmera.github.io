cc8e82f85d5c395e1ccb8d216c6523742bf425fc
Jainil Ajmera <jainil@berkeley.edu>
2023-01-09 19:27:09 +0530

    Major refactor

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-$(document).ready(function () {
-	$(".button-collapse").sideNav({
-		menuWidth: 300,
-		draggable: true
-	});
-});
+function navbarToggle() {
+	document.querySelector('.nav-cross').classList.toggle('show');
+	document.querySelector('.nav-title').classList.toggle('show');
+	document.querySelector('.nav-items').classList.toggle('show');
+};
+
+function navbarClose() {
+	document.querySelector('.nav-cross').classList.remove('show');
+	document.querySelector('.nav-title').classList.remove('show');
+	document.querySelector('.nav-items').classList.remove('show');
+};
+
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (window.scrollY > 50 || window.scrollY > 50) {
+		document.querySelector('.scroll-top').classList.add('show');
+	} else {
+		document.querySelector('.scroll-top').classList.remove('show');
+	}
+}
